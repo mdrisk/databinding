6fde97b49fc3faa58733af8b9863fdea4f4a50cb
mdrisk <mrisk@westclarkschools.com>
2018-01-31 14:17:39 -0500

    Second commit

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+  onAddBlueprint() {
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
   }
 
 }
